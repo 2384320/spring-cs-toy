8b1977a31aad6776bdcffe7f8a195d53991218e3
2384320 <chdms1549@gmail.com>
2024-01-20 22:52:33 +0900

    feat: CustomUserDetailsService 추가

---
+++ src/main/java/com/project/cs/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.project.cs.security;
+
+import com.project.cs.exception.ErrorCode;
+import com.project.cs.exception.SVCException;
+import com.project.cs.member.domain.entity.Member;
+import com.project.cs.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new SVCException(ErrorCode.MEMBER_ERROR_NOT_FOUND));
+        return CustomUserDetails.createCustomUserDetails(member);
+    }
+}
