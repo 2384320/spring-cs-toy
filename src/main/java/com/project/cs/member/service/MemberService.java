4c2caa23ee0c7664527855acab968fdaefd3c384
2384320 <chdms1549@gmail.com>
2024-01-20 22:56:03 +0900

    feat: MemberService 추가

---
+++ src/main/java/com/project/cs/member/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.project.cs.member.service;
+
+import com.project.cs.member.domain.entity.Member;
+import com.project.cs.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Optional<Member> findByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+}
