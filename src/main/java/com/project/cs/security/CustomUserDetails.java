4597b47bcd374ba2bb8479b038432ce894f276e9
2384320 <chdms1549@gmail.com>
2024-01-20 22:52:25 +0900

    feat: CustomUserDetails 추가

---
+++ src/main/java/com/project/cs/security/CustomUserDetails.java
@@ -0,0 +1,71 @@
+package com.project.cs.security;
+
+import com.project.cs.member.domain.entity.Member;
+import com.project.cs.member.domain.enums.MemberRole;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class CustomUserDetails implements UserDetails {
+
+    private Long memberId;
+    private String email;
+    private String password;
+    private MemberRole role;
+
+    public static CustomUserDetails createCustomUserDetails(Member member) {
+        return CustomUserDetails.builder()
+                .memberId(member.getId())
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .role(member.getRole())
+                .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
