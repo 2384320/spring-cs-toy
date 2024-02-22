96096f576f288163861b25b9c3491e33419f19d1
2384320 <chdms1549@gmail.com>
2024-02-23 01:58:02 +0900

    refactor: CommonResponse 반영

--- src/main/java/com/project/cs/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/cs/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.project.cs.common.exception;
 
+import com.project.cs.common.response.CommonResponse;
+import com.project.cs.common.response.code.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,48 +10,40 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.annotation.PostConstruct;
-import java.util.Arrays;
-
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    private ErrorCode[] errorCodes;
-
-    @PostConstruct
-    public void init() {
-        errorCodes = ErrorCode.values();
-    }
-
     @ExceptionHandler
-    public ResponseEntity serverExceptionHandler(Exception e) {
+    public ResponseEntity<Object> serverExceptionHandler(Exception e) {
         log.error("정의되지 않거나 서버 에러: {}", e.getMessage(), e);
 
-        ResultData resultData;
+        CommonResponse response;
         if (e instanceof HttpMessageNotReadableException)
-            resultData = new ResultData(ErrorCode.SERVER_ERROR_JSON_PARSE);
+            response = CommonResponse.failure(ErrorCode.SERVER_ERROR_JSON_PARSE);
         else
-            resultData = new ResultData(ErrorCode.SERVER_ERROR_UNDEFINED);
-        return new ResponseEntity<>(resultData, HttpStatus.INTERNAL_SERVER_ERROR);
+            response = CommonResponse.failure(ErrorCode.SERVER_ERROR_UNDEFINED);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
-    public ResponseEntity serviceExceptionHandler(SVCException e) {
-        ResultData resultData;
-
-        if (Arrays.stream(errorCodes).anyMatch(errorCode -> errorCode == e.getErrorCode()))
-            resultData = new ResultData(e.getErrorCode());
-        else {
-            log.error("정의되지 않은 에러: {}", e.getMessage(), e);
-            return new ResponseEntity<>(new ResultData(ErrorCode.SERVER_ERROR_UNDEFINED), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        return new ResponseEntity<>(resultData, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> serviceExceptionHandler(SVCException e) {
+        return new ResponseEntity<>(
+                CommonResponse.failure(
+                        e.getErrorCode()
+                ),
+                HttpStatus.BAD_REQUEST
+        );
     }
 
     @ExceptionHandler
-    public ResponseEntity validExceptionHandler(MethodArgumentNotValidException e) {
-        return new ResponseEntity<>(new ResultData(ErrorCode.INPUT_ERROR_VALIDATION, e.getBindingResult()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> validExceptionHandler(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(
+                CommonResponse.failure(
+                        ErrorCode.INPUT_ERROR_VALIDATION,
+                        e.getBindingResult()
+                ),
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
