05bc933fa0cddd7c9436c5097902edf34dab7d00
2384320 <chdms1549@gmail.com>
2024-02-23 01:26:21 +0900

    feat: 커스텀 예외 처리 제작

---
+++ src/main/java/com/project/cs/common/security/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.project.cs.common.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.cs.common.response.CommonResponse;
+import com.project.cs.common.response.code.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(
+                response.getWriter(),
+                CommonResponse.failure(ErrorCode.MEMBER_INVALID_TOKEN)
+        );
+    }
+}
